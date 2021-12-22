bb6e9b81eb206c8cae8015eb801257cefde542ab
Javier <jacevesh21@gmail.com>
2021-12-21 23:16:24 -0600

    styles almost done

--- src/components/Sauces/index.js
+++ src/components/Sauces/index.js
@@ -30,9 +30,9 @@ export default function Sauces() {
             <div class="bg-white">
             <div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
                 <div class="text-center">
-                <h2 class="text-base font-semibold text-indigo-600 tracking-wide uppercase">Products</h2>
-                <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">All Sauces</p>
-                <p class="max-w-xl mt-5 mx-auto text-xl text-gray-500">Start building for free, then add a site plan to go live. Account plans unlock additional features.</p>
+                <h2 class="text-base font-semibold text-red-600 tracking-wide uppercase">Products</h2>
+                <p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">Salsa Picante de mi Barrio</p>
+                <p class="max-w-xl mt-5 mx-auto text-xl text-green-600">Start building for free, then add a site plan to go live. Account plans unlock additional features.</p>
                 </div>
             </div>
             </div>
